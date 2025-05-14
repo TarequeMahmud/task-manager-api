1ad24758fc6531a2e4379447244cf17f8b70304f
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-05-14 18:58:53 +0600

    feat: add CRUD activities for tasks in the task manager app
    - Added DTOs for creating and updating tasks.
    - Implemented tasks controller with unit tests.
    - Created tasks service with unit tests.
    - Defined tasks model and module.

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Param,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  getTaskById(id: string) {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    if (!task) {
+      throw new Error(`Task with ID "${id}" not found`);
+    }
+    task.status = status;
+    return task;
+  }
+}
